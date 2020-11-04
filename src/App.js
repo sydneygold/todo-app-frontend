1bc43a80fc49e2c35627c9e06794e08ae582b8ee
sydneygold <sydney.goldberg.com>
2020-11-04 14:36:00 -0700

    made persistent create todo and optimistic delete button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { Component } from 'react';
 import './App.css';
+import TodoContainer from './components/TodoContainer'
+import TodoForm from './components/TodoForm'
+
+const baseUrl = 'http://localhost:3000/todos'
 
 class App extends Component {
 
@@ -7,12 +11,40 @@ class App extends Component {
     todos: []
   }
 
-  
+  componentDidMount(){
+    this.getTodos()
+  }
+
+  getTodos = () => {
+    fetch(baseUrl)
+    .then(response => response.json())
+    .then(todos => this.setState({todos}))
+  }
+
+  addTodo = (newTodo) => {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+    fetch(baseUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(newTodo)
+    })
+  }
+
+  deleteTodo = (id) => {
+    let filtered = this.state.todos.filter(todo => todo.id !== id)
+    this.setState({
+      todos: filtered
+    })
+  }
 
   render(){
     return (
       <div className="App">
         <h1>Todo App</h1>
+        <TodoForm addTodo={this.addTodo}/>
+        <TodoContainer todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
